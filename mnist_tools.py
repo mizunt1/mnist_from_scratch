53d001bdfc8a9cf4e676db65f44e80202f7c1222
Mizu Nishikawa-Toomey <Mizunt@Mizus-MacBook-Pro.local>
2018-09-26 18:57:28 +0100

    added tools

--- mnist_tools.py
+++ mnist_tools.py
@@ -1,7 +1,13 @@
 from PIL import Image
-def get_pixels()
-    im = Image.open('data/testSample/img_339.jpg')
-    pixels = list(im.getdata())
+import numpy as np
+
+def get_pixels(file_name):
+    im = Image.open(file_name)
+    pixels = np.asarray(im.getdata())
     width, height = im.size
-    print(pixels)
+    pixels = np.reshape(pixels, (width,height))
+    return pixels
 # returns 1d pixel, better to return 2d pixel
+
+if __name__=="__main__":
+    print(get_pixels("data/testSample/img_347.jpg"))
