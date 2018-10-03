8b817ea60a24399346d69b78905f27d84346f6e4
Mizu Nishikawa-Toomey <mizu.nishikawa-toomey@nanoporetech.com>
2018-10-03 13:15:52 +0100

    distinction between fully connected and not

--- mnist_tools.py
+++ mnist_tools.py
@@ -1,13 +1,18 @@
 from PIL import Image
 import numpy as np
 
+
 def get_pixels(file_name):
+    """
+    takes a file path to an image and returns an array of pixels
+    """
     im = Image.open(file_name)
     pixels = np.asarray(im.getdata())
     width, height = im.size
-    pixels = np.reshape(pixels, (width,height))
+    pixels = np.reshape(pixels, (width, height))
     return pixels
-# returns 1d pixel, better to return 2d pixel
+
+
 
 if __name__=="__main__":
     print(get_pixels("data/testSample/img_347.jpg"))
