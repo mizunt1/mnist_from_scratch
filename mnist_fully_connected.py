9e59b3c4621042a6cd5e96991965e546a8ea1f69
Mizu Nishikawa-Toomey <mizu.nishikawa-toomey@nanoporetech.com>
2018-11-06 11:28:01 +0000

    renamed stuff

--- mnist_fully_connected.py
+++ mnist_fully_connected.py
@@ -22,12 +22,15 @@ pixels = mt.get_pixels(pixel_file).flatten()
 one_hot = np.array([0 for i in range(10)])
 one_hot[0] = 1
 
-layer1_weight = mt.weight_2d(input_len, layer1_len)
+input_to_h1_weight = mt.weight_2d(input_len, layer1_len)
 # (784, 100)
-layer2_weight = mt.weight_2d(layer1_len, layer2_len)
+h1_to_h2_weight = mt.weight_2d(layer1_len, layer2_len)
 # (100, 80)
-output_weight = mt.weight_2d(layer2_len, output_len)
+h2_to_out_weight = mt.weight_2d(layer2_len, output_len)
 # (80,10)
+starting_weights = {"input_to_h1_weight": input_to_h1_weight,
+                    "h1_to_h2_weight": h1_to_h2_weight,
+                    "h2_to_out_weight": h2_to_out_weight}
 
 # biases
 
@@ -36,24 +39,25 @@ layer2_bias = mt.bias(layer2_len)
 output_bias = mt.bias(output_len)
 
 # model
-layer1_out = mt.sigmoid(np.matmul(pixels, layer1_weight))
-layer2_out = mt.sigmoid(np.matmul(layer1_out, layer2_weight))
-final_out = mt.sigmoid(np.matmul(layer2_out, output_weight))
+
 
 def run_model(input_vector, target, input_to_h1_weight, h1_to_h2_weight,
               h2_to_out_weight):
     input_layer_out = mt.relu(np.matmul(input_vector, input_to_h1_weight))
-    h1_out = mt.relu(np.matmul(input_layer_out, h1_to_h1_weight))
-    h2_out = mt.softmax(np.matmul(layer1_out, h2_to_out_weight))
+    h1_out = mt.relu(np.matmul(input_layer_out, h1_to_h2_weight))
+    h2_out = mt.softmax(np.matmul(h1_out, h2_to_out_weight))
     final_out = mt.loss(target, h2_out)
     return final_out, h2_out, h1_out, input_layer_out
+
 
 def run_back_prop(iterations, input_data, target, starting_weights):
     """
     rememebr that this is still for 1D
     """
     # starting weights dictionary thing:
-    input_to_h1_weight = starting_weights[input_to_h1_weigh]
+    input_to_h1_weight = starting_weights['input_to_h1_weight']
+    h1_to_h2_weight = starting_weights['new_h1_to_h2_weight']
+    h2_to_out_weight = starting_weights['new_h2_to_out_weight']
     for i in range(iterations):
         if i != 0:
             input_to_h1_weight = new_input_to_h1_weight
@@ -67,9 +71,9 @@ def run_back_prop(iterations, input_data, target, starting_weights):
         new_h2_f_weights = mt.hidden_to_final(
             h2_to_out_weight, h2_out, final_out, target)
         new_h1_h2_weights = mt.hidden_1_to_hidden_2(h2)
+
 loss = mt.loss(one_hot, final_out)
 
 loss_ave = np.average(loss)
 print(loss)
 print(loss_ave)
-
